e859a826cf704f50ce6e0bb13c2d920b2bbfbf7a
GanizaniSitara <dcx45@hotmail.com>
2024-02-06 14:56:06 +0000

    Pretty print XML only in DEBUG.

--- drawio_application_landscape/pyMX.py
+++ drawio_application_landscape/pyMX.py
@@ -851,11 +851,13 @@ def render_L1(file):
 
     file_name = file[:-4]
 
+    logger.info(f"Saving to {file_name}.drawio")
     xml_to_file(mxGraphModel, file_name + '.drawio')
 
-    drawio_shared_functions.pretty_print(mxGraphModel)
-    os.system(f'"{ScriptConfig.DRAWIO_PATH}" ' + file_name + ".drawio")
+    if DEBUG:
+        drawio_shared_functions.pretty_print(mxGraphModel)
 
+    os.system(f'"{ScriptConfig.DRAWIO_PATH}" ' + file_name + ".drawio")
 
 def render_partial_views(file_name, level1s):
     for level1 in level1s:
